5825e0bfd380b73fe6711f822d3f2685faae3460
Felipe Toro Castilla <ftorocastilla@gmail.com>
2022-12-23 11:46:02 -0300

    feat: implemantation of button and nav-bar

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//Enter to web
 const primaryBtn = document.getElementById("primary-button");
 primaryBtn.addEventListener("click", toggleSection);
 
 const logoBtn = document.getElementById("logo");
 logoBtn.addEventListener("click", toggleSection);
 
+//Change beetween sections from menu bar
 function toggleSection() {
   const main = document.querySelector("main");
   const header = document.querySelector("header");
@@ -17,6 +19,7 @@ function toggleSection() {
   }, 1000);
 }
 
+//Return to home from web
 const homeBtn = document.getElementById("goHome");
 homeBtn.addEventListener("click", goHome);
 
@@ -26,5 +29,15 @@ function goHome() {
 
   main.setAttribute("class", "inactive");
 
-  header.classList.toggle('inactive');
+  header.classList.toggle("inactive");
+}
+
+//Active menu button
+const menu = document.getElementById("menu");
+
+menu.addEventListener("click", toggle);
+function toggle () {
+  const listMenu = document.getElementById("list-menu");
+  listMenu.classList.toggle('inactive');
+  listMenu.classList.toggle('active');
 }
