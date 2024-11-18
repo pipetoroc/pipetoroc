7cfca0ad19f0a73eb9861c58ebda82764f5503c6
Felipe Toro C <ftorocastilla@gmail.com>
2024-11-17 21:15:11 -0300

    feat: add folder Felipe-Toro as a part of original repository

---
+++ Felipe-Toro/tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				'primary-color': '#FEFFFE',
+				'dark-color': '#0a1128',
+				'secondary-color': '#067bc2',
+				'accent-color': '#ffa62b',
+				'support-color': '#37392a'
+			}
+		},
+	},
+	plugins: [],
+}
